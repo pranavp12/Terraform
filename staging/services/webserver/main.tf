e07fadc8c1f2bff5da89baf3288b1fd4ba3d931f
pranavp12 <phalnikar.pranav@gmail.com>
2021-05-09 21:32:59 +0530

    Add files via upload

---
+++ staging/services/webserver/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "webservers" {
+  source = "../../../modules/services/webservers"
+  cluster_name = "webservers-dev"
+  instance_type = "t2.nano"
+  min_size      = 1
+  max_size      = 5
+  desired_capacity  = 3
+}    
