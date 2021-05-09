5194ac1ad1d38b30584a915a7ff7cf5b4ceccbdc
pranavp12 <phalnikar.pranav@gmail.com>
2021-05-09 21:29:46 +0530

    Add files via upload

---
+++ modules/services/webserver/variables.tf
@@ -0,0 +1,36 @@
+variable "server_port" {
+  description = "The port the web server will be listening"
+  type        = number
+  default     = 8080
+}
+
+variable "elb_port" {
+  description = "The port the elb will be listening"
+  type        = number
+  default     = 80
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+  type        = number
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+  type        = number
+}
+
+variable "desired_capacity" {
+  description = "The desired number of EC2 Instances in the ASG"
+  type        = number
+}
