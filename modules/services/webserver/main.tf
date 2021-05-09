d442bd8c201088c80d9b993d47a76a67e056e6d7
pranavp12 <phalnikar.pranav@gmail.com>
2021-05-09 21:29:25 +0530

    Create main.tf

---
+++ modules/services/webserver/main.tf
@@ -0,0 +1,87 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_launch_configuration" "asg-launch-config-sample" {
+  image_id          = "ami-07ebfd5b3428b6f4d"
+  instance_type = var.instance_type
+  security_groups = [aws_security_group.busybox.id]
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, Terraform & AWS ASG" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "busybox" {
+  name = "${var.cluster_name}-busybox-sg"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "elb-sg" {
+  name = "${var.cluster_name}-elb-sg"
+  # Allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.elb_port
+    to_port     = var.elb_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_autoscaling_group" "asg-sample" {
+  launch_configuration = aws_launch_configuration.asg-launch-config-sample.id
+  availability_zones   = data.aws_availability_zones.all.names
+  min_size = var.min_size
+  max_size = var.max_size
+  desired_capacity = var.desired_capacity
+
+  load_balancers    = [aws_elb.sample.name]
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "${var.cluster_name}-asg"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "sample" {
+  name               = "${var.cluster_name}-asg-elb"
+  security_groups    = [aws_security_group.elb-sg.id]
+  availability_zones = data.aws_availability_zones.all.names
+
+  health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  # Adding a listener for incoming HTTP requests.
+  listener {
+    lb_port           = var.elb_port
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
