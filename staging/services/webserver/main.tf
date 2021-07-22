4ffce40d733a2a24a71b53962400eef860c1de9f
pranavp12 <phalnikar.pranav@gmail.com>
2021-07-22 22:19:45 +0530

    Update main.tf

--- staging/services/webserver/main.tf
+++ staging/services/webserver/main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 module "webservers" {
   source = "../../../modules/services/webserver"
-  cluster_name = "webservers-dev"
+  cluster_name = "webservers-staging"
   instance_type = "t2.nano"
   min_size      = 1
-  max_size      = 5
+  max_size      = 3
   desired_capacity  = 1
 }    
